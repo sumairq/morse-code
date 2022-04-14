ebe454469876a6179b2e58c43683e6b70b6a907b
Dennis Akagha <d.akagha20@gmail.com>
2022-04-14 23:55:25 +0100

    Add the sentence method

--- morse.rb
+++ morse.rb
@@ -33,3 +33,26 @@ MORSE = {
         return key.upcase if value == char
     }
      end 
+
+     def decode_word(word)
+      new_word = ''
+      new_arr = word.split
+      new_arr.each {|value|
+      new_word += decode_char(value)
+      }
+      return new_word 
+  end 
+  
+  # Sentence method
+  
+  def decode_sentence(sentence)
+      new_sentence = ''
+      new_arr = sentence.split('   ')
+      new_arr.each {|value|
+      new_sentence += "#{decode_word(value)} "
+      }
+      return new_sentence
+  end
+  
+  puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+  
